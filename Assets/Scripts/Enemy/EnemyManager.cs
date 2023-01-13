0f11ee069a65f7d60e877d97bce9ef82123e939e
Sam Benn <sam-benn@hotmail.co.uk>
2023-01-13 22:37:50 +0000

    tracked entities carry over fix

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -66,7 +66,7 @@ public class EnemyManager : MonoBehaviour
 
             if(current != null)
             {
-                current.AddToTrackedEntities(current.GetTracked());
+                current.AddToTrackedEntities(old.GetTracked());
             }
         });
 
