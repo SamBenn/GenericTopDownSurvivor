df73971d9c5247d955a5f3a6856277934f17631a
Sam Benn <sam-benn@hotmail.co.uk>
2023-01-14 10:21:22 +0000

    Weighted random

--- Assets/Scripts/Utilities/Random.cs
+++ Assets/Scripts/Utilities/Random.cs
@@ -9,17 +9,47 @@ public static class RandomUtil
     {
         int[] result = new int[count];
         List<int> numbersInOrder = new List<int>();
-        for (var x = min; x < max; x++)
+        for (var i = min; i < max; i++)
         {
-            numbersInOrder.Add(x);
+            numbersInOrder.Add(i);
         }
-        for (var x = 0; x < count; x++)
+        for (var i = 0; i < count; i++)
         {
             var randomIndex = UnityEngine.Random.Range(0, numbersInOrder.Count);
-            result[x] = numbersInOrder[randomIndex];
+            result[i] = numbersInOrder[randomIndex];
             numbersInOrder.RemoveAt(randomIndex);
         }
 
         return result.ToList();
     }
+
+    public static List<int> UniqueWeightedRandomsBetween(List<IWeighted> weights, int count)
+    {
+        int[] toReturn = new int[count];
+        List<int> indexes = new List<int>();
+
+        for (var i = 0; i < weights.Count; i++)
+        {
+            // pretty literal which isn't great, would like to take weights as percentage of whole an apply to 
+            for (int w = 0; w < weights[i].Weight; w++)
+            {
+                indexes.Add(i);
+            }
+        }
+        for (var i = 0; i < count; i++)
+        {
+            var randomIndex = UnityEngine.Random.Range(0, indexes.Count);
+            var result = indexes[randomIndex];
+            toReturn[i] = result;
+            // probably horifically inneficient but it should work
+            indexes.RemoveAll(p => p == result);
+        }
+
+        return toReturn.ToList();
+    }
+}
+
+public interface IWeighted
+{
+    public float Weight { get; set; }
 }
